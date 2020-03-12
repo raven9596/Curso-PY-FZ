7478db63652e55ee5c6bdd1503f31a1433b7d0ff
Gilberto Acosta <acostagilberto95@gmail.com>
2020-03-12 16:02:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Curso Python
+
+![Python](https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/267_Python_logo-512.png)
+
+Este repositorio es para guardar mis archivos del curso de Python
---
+++ datatype.py
@@ -0,0 +1,34 @@
+# String
+print("Hello World")
+print('Hello World')
+print('''Hello World''')
+print("""Hello World""")
+
+
+# Numbers
+print(30) # Integer
+print(30.5) # Float
+
+
+# Boolean
+True
+False
+
+
+# List
+[10, 20, 30, 55]
+['Hello', 'Bye', "Adios"]
+[10, "Hello", True, 10.1]
+[]
+
+# Tuples
+(10, 20, 30, 55)
+()
+
+# Dictionaries
+{
+    "name":"Ryan",
+    "lastname":"Ray",
+    "nickname":"Fazt"
+}
+
